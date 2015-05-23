228860d102386fc34211f0eb3e984c9c511b9ea5
Aaron Tidwell <Aaron.Tidwell@gmail.com>
2015-05-23 00:31:00 -0400

    reorg to json file

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 var fs = require('fs');
 var path = require('path');
 
+function findByDomain(services, domain) {
+	var service;
+	for (service in services) {
+		if (services.hasOwnProperty(service) && services[service].domain === domain) {
+			return services[service];
+		}
+	}
+	return false;
+}
+
 function get(env) {
 	if (!env) { env = 'local'; }
 
-	var cert = {
-		key: fs.readFileSync(path.join(__dirname, './sslcert/' + env + '.key'), 'utf8'),
-		cert: fs.readFileSync(path.join(__dirname, './sslcert/' + env + '.cert'), 'utf8')
-	};
 	var services = require('./env/' + env);
 
-	function findByDomain(domain) {
-		var service;
-		for (service in services) {
-			if (services.hasOwnProperty(service) && services[service].domain === domain) {
-				return services[service];
-			}
-		}
-		return false;
-	}
+	var cert = {
+		key: fs.readFileSync(path.join(__dirname, './sslcert/' + services.key), 'utf8'),
+		cert: fs.readFileSync(path.join(__dirname, './sslcert/' + services.cert), 'utf8')
+	};
 
 	return {
 		services: services,
 		cert: cert,
-		findByDomain: findByDomain
+		findByDomain: function(domain) { return findByDomain(services, domain); }
 	};
 }
 
