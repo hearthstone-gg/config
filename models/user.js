2d2d32a206f99888a169a306ad51e214715505c2
Aaron Tidwell <Aaron.Tidwell@gmail.com>
2015-09-06 00:10:50 -0400

    fix model

--- models/user.js
+++ models/user.js
@@ -1,32 +1,34 @@
 var uuid = require('node-uuid');
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
 
-var userSchema = new Schema({
-	bnet: {
-		id: String,
-		token: String,
-		name: String
-	},
-	token: String
-});
+function UserModel(mongoose) {
+	var Schema = mongoose.Schema;
 
-userSchema.methods.getDisplayName = function() {
-	var name = '';
-	if (this.bnet.name) {
-		name = this.bnet.name.split('#')[0];
-	}
-	return name.toLowerCase();
-}
-
-userSchema.methods.generateToken = function(cb){
-	//TODO use bnet token in hash
-	this.token = uuid.v4();
-	this.save(function() {
-		if (cb) { cb(); }
+	var userSchema = new Schema({
+		bnet: {
+			id: String,
+			token: String,
+			name: String
+		},
+		token: String
 	});
-};
 
-var UserModel = mongoose.model('User', userSchema);
+	userSchema.methods.getDisplayName = function() {
+		var name = '';
+		if (this.bnet.name) {
+			name = this.bnet.name.split('#')[0];
+		}
+		return name.toLowerCase();
+	};
+
+	userSchema.methods.generateToken = function(cb){
+		//TODO use bnet token in hash
+		this.token = uuid.v4();
+		this.save(function() {
+			if (cb) { cb(); }
+		});
+	};
+
+	return mongoose.model('User', userSchema);
+}
 
 module.exports = UserModel;
